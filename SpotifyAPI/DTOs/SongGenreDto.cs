3cdbfea1fd797623b63b860926421a1844400963
LDThanh259 <ducthanh2592004@gmail.com>
2025-05-27 00:20:39 +0700

    endpoint song-genre

---
+++ SpotifyAPI/DTOs/SongGenreDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SpotifyAPI.DTOs
+{
+    public class SongGenreDto
+    {
+        public int SongId { get; set; }
+        public int GenreId { get; set; }
+    }
+    public class SongGenreDetailDto
+    {
+        public int SongId { get; set; }
+        public string SongTitle { get; set; }
+
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+    }
+
+}
