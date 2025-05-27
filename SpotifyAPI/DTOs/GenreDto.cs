b1df4448483399bf66150b1a85f5f401524a6589
LDThanh259 <ducthanh2592004@gmail.com>
2025-05-27 08:42:49 +0700

    fix: resolved conflic genre service

---
+++ SpotifyAPI/DTOs/GenreDto.cs
@@ -0,0 +1,21 @@
+﻿using SpotifyAPI.Models;
+
+namespace SpotifyAPI.DTOs
+{
+    public class GenreDto
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+        public IEnumerable<SongDto> Songs { get; set; }
+    }
+
+    public class CreateGenreDto
+    {
+        public string GenreName { get; set; }
+    }
+
+    public class UpdateGenreDto
+    {
+        public string GenreName { get; set; }
+    }
+}
