07be2fe50e1e29524c3033a7813fe662a393c5a5
LDThanh259 <ducthanh2592004@gmail.com>
2025-05-28 18:41:02 +0700

    Update GenreController to support image in API

--- SpotifyAPI/Controllers/GenreController.cs
+++ SpotifyAPI/Controllers/GenreController.cs
@@ -1,37 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using SpotifyAPI.Services;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using SpotifyAPI.Services;
 
-namespace SpotifyAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class GenreController : ControllerBase
-    {
-        private readonly IGenreService _genreService;
+//namespace SpotifyAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class GenreController : ControllerBase
+//    {
+//        private readonly IGenreService _genreService;
 
-        public GenreController(IGenreService genreService)
-        {
-            _genreService = genreService;
-        }
+//        public GenreController(IGenreService genreService)
+//        {
+//            _genreService = genreService;
+//        }
 
-        [HttpGet("{id}/songs")]
-        public async Task<IActionResult> GetSongsByGenre(int id)
-        {
-            var songs = await _genreService.GetSongsByGenreIdAsync(id);
-            if (songs == null || !songs.Any())
-            {
-                return NotFound($"No songs found for GenreId: {id}");
-            }
+//        [HttpGet("{id}/songs")]
+//        public async Task<IActionResult> GetSongsByGenre(int id)
+//        {
+//            var songs = await _genreService.GetSongsByGenreIdAsync(id);
+//            if (songs == null || !songs.Any())
+//            {
+//                return NotFound($"No songs found for GenreId: {id}");
+//            }
 
-            return Ok(songs);
-        }
+//            return Ok(songs);
+//        }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
-        {
-            var genres = await _genreService.GetAllGenresAsync();
-            return Ok(genres);
-        }
-    }
-}
+//        [HttpGet]
+//        public async Task<IActionResult> GetAll()
+//        {
+//            var genres = await _genreService.GetAllGenresAsync();
+//            return Ok(genres);
+//        }
+//    }
+//}
